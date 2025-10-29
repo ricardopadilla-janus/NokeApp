3fde83a36dc18df7ef706d49d5fa461b4c4c904d
Ricardo padilla <ricardo.padilla@janusintl.com>
2025-10-29 15:58:42 -0400

    pointing to noke-inc library, adding bundler to avoid ruby issues.

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,17 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const workspaceRoot = path.resolve(__dirname, '..');
+const coreLibPath = '/Users/ricardo.padilla/Documents/Noke/noke-react-native-core';
+
+const config = {
+  watchFolders: [workspaceRoot, coreLibPath],
+  resolver: {
+    extraNodeModules: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
